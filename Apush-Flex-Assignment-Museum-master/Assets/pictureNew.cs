d41c2838b82f8b76569b4052524ed6106e0c659d
GrahamNick <37307748+GrahamNick@users.noreply.github.com>
2019-03-12 23:14:37 -0400

    thgfd

---
+++ Apush-Flex-Assignment-Museum-master/Assets/pictureNew.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pictureNew : MonoBehaviour {
+	public GameObject prefab;
+	private GameObject clone;
+
+
+	void OnTriggerEnter(Collider other) {
+		clone = Instantiate (prefab);
+	}
+	void OnTriggerExit(Collider other) {
+		Destroy (clone,0.25f);
+	}
+}
